4109128694ec43b09dfc49617c2f71f383308677
Alexander Dudko <adudko@yelp.com>
2017-09-05 12:13:15 -0700

    check_marathon_services_replication with PaastaServiceConfig

--- paasta_tools/check_marathon_services_replication.py
+++ paasta_tools/check_marathon_services_replication.py
@@ -32,6 +32,7 @@ instance then it will be used instead.
 """
 import argparse
 import logging
+import os
 from datetime import datetime
 from datetime import timedelta
 
@@ -40,12 +41,12 @@ import pysensu_yelp
 from paasta_tools import marathon_tools
 from paasta_tools import monitoring_tools
 from paasta_tools.marathon_tools import format_job_id
+from paasta_tools.paasta_service_config import PaastaServiceConfig
 from paasta_tools.smartstack_tools import load_smartstack_info_for_service
 from paasta_tools.utils import _log
 from paasta_tools.utils import compose_job_id
 from paasta_tools.utils import datetime_from_utc_to_local
 from paasta_tools.utils import DEFAULT_SOA_DIR
-from paasta_tools.utils import get_services_for_cluster
 from paasta_tools.utils import is_under_replicated
 from paasta_tools.utils import load_system_paasta_config
 from paasta_tools.utils import NoDeploymentsAvailable
@@ -54,36 +55,35 @@ from paasta_tools.utils import NoDeploymentsAvailable
 log = logging.getLogger(__name__)
 
 
-def send_event(service, namespace, cluster, soa_dir, status, output):
+def send_event(instance_config, status, output):
     """Send an event to sensu via pysensu_yelp with the given information.
 
-    :param service: The service name the event is about
-    :param namespace: The namespace of the service the event is about
-    :param soa_dir: The service directory to read monitoring information from
+    :param instance_config: An instance of MarathonServiceConfig
     :param status: The status to emit for this event
     :param output: The output to emit for this event"""
     # This function assumes the input is a string like "mumble.main"
-    monitoring_overrides = marathon_tools.load_marathon_service_config(
-        service=service,
-        instance=namespace,
-        cluster=cluster,
-        soa_dir=soa_dir,
-        load_deployments=False,
-    ).get_monitoring()
+    monitoring_overrides = instance_config.get_monitoring()
     if 'alert_after' not in monitoring_overrides:
         monitoring_overrides['alert_after'] = '2m'
     monitoring_overrides['check_every'] = '1m'
-    monitoring_overrides['runbook'] = monitoring_tools.get_runbook(monitoring_overrides, service, soa_dir=soa_dir)
+    monitoring_overrides['runbook'] = monitoring_tools.get_runbook(
+        monitoring_overrides,
+        instance_config.service, soa_dir=instance_config.soa_dir,
+    )
 
-    check_name = 'check_marathon_services_replication.%s' % compose_job_id(service, namespace)
-    monitoring_tools.send_event(service, check_name, monitoring_overrides, status, output, soa_dir)
+    check_name = ('check_marathon_services_replication.%s' %
+                  compose_job_id(instance_config.service, instance_config.instance))
+    monitoring_tools.send_event(
+        instance_config.service, check_name, monitoring_overrides,
+        status, output, instance_config.soa_dir,
+    )
     _log(
-        service=service,
+        service=instance_config.service,
         line='Replication: %s' % output,
         component='monitoring',
         level='debug',
-        cluster=cluster,
-        instance=namespace,
+        cluster=instance_config.cluster,
+        instance=instance_config.instance,
     )
 
 
@@ -106,10 +106,7 @@ def parse_args():
 
 
 def check_smartstack_replication_for_instance(
-    service,
-    instance,
-    cluster,
-    soa_dir,
+    instance_config,
     expected_count,
     system_paasta_config,
 ):
@@ -117,16 +114,13 @@ def check_smartstack_replication_for_instance(
     emitting events to Sensu based on the fraction available and the thresholds defined in
     the corresponding yelpsoa config.
 
-    :param service: A string like example_service
-    :param instance: A PaaSTA instance, like "main"
-    :param cluster: name of the cluster
-    :param soa_dir: The SOA configuration directory to read from
+    :param instance_config: An instance of MarathonServiceConfig
     :param system_paasta_config: A SystemPaastaConfig object representing the system configuration.
     """
-    full_name = compose_job_id(service, instance)
+    full_name = compose_job_id(instance_config.service, instance_config.instance)
 
     primary_registration = marathon_tools.read_registration_for_service_instance(
-        service, instance, soa_dir=soa_dir,
+        instance_config.service, instance_config.instance, soa_dir=instance_config.soa_dir,
     )
 
     if primary_registration != full_name:
@@ -136,16 +130,15 @@ def check_smartstack_replication_for_instance(
         )
         return
 
-    job_config = marathon_tools.load_marathon_service_config(service, instance, cluster)
-    crit_threshold = job_config.get_replication_crit_percentage()
-    monitoring_blacklist = job_config.get_monitoring_blacklist(
+    crit_threshold = instance_config.get_replication_crit_percentage()
+    monitoring_blacklist = instance_config.get_monitoring_blacklist(
         system_deploy_blacklist=system_paasta_config.get_deploy_blacklist(),
     )
     log.info('Checking instance %s in smartstack', full_name)
     smartstack_replication_info = load_smartstack_info_for_service(
-        service=service,
-        namespace=instance,
-        soa_dir=soa_dir,
+        service=instance_config.service,
+        namespace=instance_config.instance,
+        soa_dir=instance_config.soa_dir,
         blacklist=monitoring_blacklist,
         system_paasta_config=system_paasta_config,
     )
@@ -216,15 +209,15 @@ def check_smartstack_replication_for_instance(
                 "  * Increase the instance count\n"
                 "\n"
             ) % {
-                'service': service,
-                'instance': instance,
-                'cluster': cluster,
+                'service': instance_config.service,
+                'instance': instance_config.instance,
+                'cluster': instance_config.cluster,
             }
             log.error(output)
         else:
             status = pysensu_yelp.Status.OK
             log.info(output)
-    send_event(service=service, namespace=instance, cluster=cluster, soa_dir=soa_dir, status=status, output=output)
+    send_event(instance_config=instance_config, status=status, output=output)
 
 
 def filter_healthy_marathon_instances_for_short_app_id(all_tasks, app_id):
@@ -235,44 +228,38 @@ def filter_healthy_marathon_instances_for_short_app_id(all_tasks, app_id):
     for task in tasks_for_app:
         if task.started_at is not None:
             print(datetime_from_utc_to_local(task.started_at))
-        if marathon_tools.is_task_healthy(task, default_healthy=True) \
-                and task.started_at is not None \
-                and datetime_from_utc_to_local(task.started_at) < one_minute_ago:
+        if (
+            marathon_tools.is_task_healthy(task, default_healthy=True)
+            and task.started_at is not None
+            and datetime_from_utc_to_local(task.started_at) < one_minute_ago
+        ):
             healthy_tasks.append(task)
     return len(healthy_tasks)
 
 
 def check_healthy_marathon_tasks_for_service_instance(
-    service, instance, cluster,
-    soa_dir, expected_count, all_tasks,
+    instance_config, expected_count, all_tasks,
 ):
-    app_id = format_job_id(service, instance)
+    app_id = format_job_id(instance_config.service, instance_config.instance)
     num_healthy_tasks = filter_healthy_marathon_instances_for_short_app_id(
         all_tasks=all_tasks,
         app_id=app_id,
     )
     log.info("Checking %s in marathon as it is not in smartstack" % app_id)
     send_event_if_under_replication(
-        service=service,
-        instance=instance,
-        cluster=cluster,
+        instance_config=instance_config,
         expected_count=expected_count,
         num_available=num_healthy_tasks,
-        soa_dir=soa_dir,
     )
 
 
 def send_event_if_under_replication(
-    service,
-    instance,
-    cluster,
+    instance_config,
     expected_count,
     num_available,
-    soa_dir,
 ):
-    full_name = compose_job_id(service, instance)
-    job_config = marathon_tools.load_marathon_service_config(service, instance, cluster)
-    crit_threshold = job_config.get_replication_crit_percentage()
+    full_name = compose_job_id(instance_config.service, instance_config.instance)
+    crit_threshold = instance_config.get_replication_crit_percentage()
     output = (
         'Service %s has %d out of %d expected instances available!\n' +
         '(threshold: %d%%)'
@@ -298,9 +285,9 @@ def send_event_if_under_replication(
             "\n"
             "      paasta status -s %(service)s -i %(instance)s -c %(cluster)s -vv\n"
         ) % {
-            'service': service,
-            'instance': instance,
-            'cluster': cluster,
+            'service': instance_config.service,
+            'instance': instance_config.instance,
+            'cluster': instance_config.cluster,
         }
         log.error(output)
         status = pysensu_yelp.Status.CRITICAL
@@ -308,59 +295,59 @@ def send_event_if_under_replication(
         log.info(output)
         status = pysensu_yelp.Status.OK
     send_event(
-        service=service,
-        namespace=instance,
-        cluster=cluster,
-        soa_dir=soa_dir,
+        instance_config=instance_config,
         status=status,
         output=output,
     )
 
 
-def check_service_replication(client, service, instance, all_tasks, cluster, soa_dir, system_paasta_config):
+def check_service_replication(instance_config, all_tasks, system_paasta_config):
     """Checks a service's replication levels based on how the service's replication
     should be monitored. (smartstack or mesos)
 
-    :param service: Service name, like "example_service"
-    :param instance: Instance name, like "main" or "canary"
-    :param cluster: name of the cluster
-    :param soa_dir: The SOA configuration directory to read from
+    :param instance_config: An instance of MarathonServiceConfig
     :param system_paasta_config: A SystemPaastaConfig object representing the system configuration.
     """
-    job_id = compose_job_id(service, instance)
+    job_id = compose_job_id(instance_config.service, instance_config.instance)
     try:
-        expected_count = marathon_tools.get_expected_instance_count_for_namespace(service, instance, soa_dir=soa_dir)
+        expected_count = marathon_tools.get_expected_instance_count_for_namespace(
+            instance_config.service,
+            instance_config.instance, soa_dir=instance_config.soa_dir,
+        )
     except NoDeploymentsAvailable:
         log.debug('deployments.json missing for %s. Skipping replication monitoring.' % job_id)
         return
     if expected_count is None:
         return
     log.info("Expecting %d total tasks for %s" % (expected_count, job_id))
-    proxy_port = marathon_tools.get_proxy_port_for_instance(service, instance, soa_dir=soa_dir)
+    proxy_port = marathon_tools.get_proxy_port_for_instance(
+        instance_config.service,
+        instance_config.instance, soa_dir=instance_config.soa_dir,
+    )
     if proxy_port is not None:
         check_smartstack_replication_for_instance(
-            service=service,
-            instance=instance,
-            cluster=cluster,
-            soa_dir=soa_dir,
+            instance_config=instance_config,
             expected_count=expected_count,
             system_paasta_config=system_paasta_config,
         )
     else:
         check_healthy_marathon_tasks_for_service_instance(
-            service=service,
-            instance=instance,
-            cluster=cluster,
-            soa_dir=soa_dir,
+            instance_config=instance_config,
             expected_count=expected_count,
             all_tasks=all_tasks,
         )
 
 
+def list_services(soa_dir=DEFAULT_SOA_DIR):
+    rootdir = os.path.abspath(soa_dir)
+    for name in os.listdir(rootdir):
+        if os.path.isdir(os.path.join(soa_dir, name)):
+            yield name
+
+
 def main():
 
     args = parse_args()
-    soa_dir = args.soa_dir
 
     if args.verbose:
         logging.basicConfig(level=logging.DEBUG)
@@ -369,24 +356,21 @@ def main():
 
     system_paasta_config = load_system_paasta_config()
     cluster = system_paasta_config.get_cluster()
-    service_instances = get_services_for_cluster(
-        cluster=cluster, instance_type='marathon', soa_dir=args.soa_dir,
-    )
 
     config = marathon_tools.load_marathon_config()
     client = marathon_tools.get_marathon_client(config.get_url(), config.get_username(), config.get_password())
     all_tasks = client.list_tasks()
-    for service, instance in service_instances:
-
-        check_service_replication(
-            client=client,
-            service=service,
-            instance=instance,
-            cluster=cluster,
-            all_tasks=all_tasks,
-            soa_dir=soa_dir,
-            system_paasta_config=system_paasta_config,
+    for service in list_services(args.soa_dir):
+        service_config = PaastaServiceConfig(
+            service=service, soa_dir=args.soa_dir,
+            load_deployments=False,
         )
+        for instance_config in service_config.instance_configs(cluster=cluster, instance_type='marathon'):
+            check_service_replication(
+                instance_config=instance_config,
+                all_tasks=all_tasks,
+                system_paasta_config=system_paasta_config,
+            )
 
 
 if __name__ == "__main__":
